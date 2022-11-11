86be090f73161897fff58eccb6e1e33431f45934
shri33nidhi <117985920+shri33nidhi@users.noreply.github.com>
2022-11-11 16:56:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                display.innerText = '';
+                break;
+            case 'sq':
+                    display.innerText = Math.pow(display.innerText,2);
+                    break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
